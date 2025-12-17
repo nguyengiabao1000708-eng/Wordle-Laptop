9f1e8b8a28b68e6ac84edf660b29da607525d26e
NguyenGiaBao <nguyengiabao1000708@gmail.com>
2025-12-17 10:17:52 +0700

    add them version tieng viet

--- data/file_process.py
+++ data/file_process.py
@@ -1,24 +1,55 @@
-word = []
-valid_word =[]
-all_word = word + valid_word
-n = int(input())
-with open ("5000_common_words.txt", "r") as common_wf:
-    for i in common_wf :
-        i = i.strip().upper()
-        if len(i) == n:
-            valid_word.append(i)
 
-with open("valid_word_with_length_n.txt", "w") as valid_wf:
-    valid_wf.write("\n".join(valid_word))
+def choose_common_words(n,file_input,file_output):
+    valid_word =[]
+    with open (file_input, "r") as common_wf:
+        for i in common_wf :
+            i = i.strip().upper()
+            if len(i) == n:
+                valid_word.append(i)
 
-with open ("words_alpha.txt", "r") as alpha_wf:
-    for i in alpha_wf :
-        i = i.strip().upper()
-        if len(i) == n:
-            word.append(i)
+    with open(file_output, "w") as valid_wf:
+        valid_wf.write("\n".join(valid_word))
 
-with open("word_with_length_n.txt", "w") as all_wf:
-    all_wf.write("\n".join(word))
+def choose_all_words(n,file_input,file_output):
+    word = []
+    with open (file_input, "r") as alpha_wf:
+        for i in alpha_wf :
+            i = i.strip().upper()
+            if len(i) == n:
+                word.append(i)
+
+    with open(file_output, "w") as all_wf:
+        all_wf.write("\n".join(word))
+def main():
+    while True:
+        language = input("vietnamese or english: ").lower()
+        if language == "vietnamese":
+            n = int(input("Chooose the word's length: "))   
+            choose_common_words(n,"common_words_vn.txt","valid_word_with_length_n.txt")
+            choose_all_words(n,"all_words_vn.txt","word_with_length_n.txt")
+            break
+        elif language == "english":
+            n = int(input("Chooose the word's length: "))
+            choose_common_words(n,"common_words_eng.txt","valid_word_with_length_n.txt")
+            choose_all_words(n,"all_words_eng.txt","word_with_length_n.txt")
+            break
+        else:
+            print("Unvalid, please type again!!")
+
+main()
+
+# words = []
+# with open("vi_dictionary.csv", "r") as f:
+#     line = f.readlines()
+#     print(len(line))
+#     for i in line:
+#         i = i.strip().split(",")
+#         words.append(i[0])
+#     print(words)
+# with open("word_with_length_n.txt", "w") as wf:
+#     for i in words:
+#         wf.write(f"{i}\n")
+
 
 
 
