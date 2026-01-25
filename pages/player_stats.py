6f28704af7ef94c206e2d44c376d66b8fced8571
NguyenGiaBao <nguyengiabao1000708@gmail.com>
2026-01-26 00:05:09 +0700

    random

--- pages/player_stats.py
+++ pages/player_stats.py
@@ -1,6 +1,30 @@
 from source import UserManager
 import streamlit as st
 
+def hide_sidebar():
+    st.markdown(
+        """
+        <style>
+            /* Ẩn hoàn toàn sidebar */
+            [data-testid="stSidebar"] {
+                display: none;
+            }
+            /* Ẩn luôn nút mũi tên để mở sidebar (Collapsed Control) */
+            [data-testid="collapsedControl"] {
+                display: none;
+            }
+            /* Mở rộng phần nội dung chính ra giữa màn hình khi không còn sidebar */
+            .main .block-container {
+                padding-left: 1rem;
+                padding-right: 1rem;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+hide_sidebar()
+
 username = st.session_state.get("username")
 game_over = st.session_state.get("game_over")
 is_win = st.session_state.get("is_win")
--- pages/ranking.py
+++ pages/ranking.py
@@ -1,6 +1,30 @@
 from source import UserManager
 import streamlit as st
 
+def hide_sidebar():
+    st.markdown(
+        """
+        <style>
+            /* Ẩn hoàn toàn sidebar */
+            [data-testid="stSidebar"] {
+                display: none;
+            }
+            /* Ẩn luôn nút mũi tên để mở sidebar (Collapsed Control) */
+            [data-testid="collapsedControl"] {
+                display: none;
+            }
+            /* Mở rộng phần nội dung chính ra giữa màn hình khi không còn sidebar */
+            .main .block-container {
+                padding-left: 1rem;
+                padding-right: 1rem;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+hide_sidebar()
+
 user_manager = UserManager()
 user_manager.load_data()
 st.set_page_config(page_title="Ranking", layout="centered")
